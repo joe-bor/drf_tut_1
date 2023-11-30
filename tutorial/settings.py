27920a259f06ddbd516710e1e70e3bc465839436
joe-bor <kimborlongan_08@yahoo.com>
2023-11-30 15:37:37 -0800

    adds Snippet model and SnipperSerializer

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tutorial.quickstart'
+    'tutorial.quickstart',
+    'tutorial.snippets.apps.SnippetsConfig'
 ]
 
 MIDDLEWARE = [
