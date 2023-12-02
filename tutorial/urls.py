53bf200f22c8ccc57809091b8f0511f02a0524ba
joe-bor <kimborlongan_08@yahoo.com>
2023-12-02 12:20:08 -0800

    added snippet views and url routes

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -17,13 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from tutorial.quickstart import views
 from rest_framework import routers
+from tutorial.snippets import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include("tutorial.snippets.urls")),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
