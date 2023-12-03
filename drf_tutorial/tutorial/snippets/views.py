c59f9ce4de82ba2016f7557b7bca74713427fe84
joe-bor <kimborlongan_08@yahoo.com>
2023-12-02 17:29:57 -0800

    feat: associates Snippet w/ User

--- drf_tutorial/tutorial/snippets/views.py
+++ drf_tutorial/tutorial/snippets/views.py
@@ -183,13 +183,29 @@
 
 ## generic class-based views
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    # associating users and snippets upon creation
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
     
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes =  [permissions.IsAuthenticatedOrReadOnly]
+    
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
