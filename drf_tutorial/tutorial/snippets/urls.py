76eff674c4d8378d3e8203090108075434e035ba
joe-bor <kimborlongan_08@yahoo.com>
2023-12-07 14:24:37 -0800

    refactor: explicit url routes to router w/ ViewSets

--- drf_tutorial/tutorial/snippets/urls.py
+++ drf_tutorial/tutorial/snippets/urls.py
@@ -1,14 +1,26 @@
-from django.urls import path
+from django.urls import path, include
 from snippets import views
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our Viewsets with it.
+# It'll then take care of the url conf for us
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
 
 urlpatterns = [
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail')
+    path('', include(router.urls))
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+
+#! This is what it looks like without ViewSets, when we manually configure the urls  
+    # urlpatterns = [
+    #     path('', views.api_root),
+    #     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    #     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    #     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    #     path('users/', views.UserList.as_view(), name='user-list'),
+    #     path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail')
+    # ]
+
+    # urlpatterns = format_suffix_patterns(urlpatterns)
